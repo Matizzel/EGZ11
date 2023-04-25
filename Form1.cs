598ea926e93610bc7cf8fc852dbfdfca68b70630
matiz <matiz@MATIZZELLAPTOP>
2023-04-25 12:05:33 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace EGZ11
+{
+    public partial class Form1 : Form
+    {
+        private int sum = 0;
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            sum += number;
+            count++;
+
+            if (sum >= 100)
+            {
+                MessageBox.Show($"Сумма: {sum}, кол-во чисел: {count}");
+                sum = 0;
+                count = 0;
+            }
+        }
+    }
+}
